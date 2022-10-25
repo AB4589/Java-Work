49e93c53acd761cd23f3aca7198bc38cc96a4f18
AB4589 <tarekrahat4@gmail.com>
2022-10-26 01:58:08 +0600

    This my first git upload

---
+++ pondit_task/removeDuplicate.java
@@ -0,0 +1,18 @@
+public class removeDuplicate {
+    public static void main(String[] args) {
+        int a[] = {1,2,2,3,4,5,6};
+        int j = 0;
+        for(int i=0; i<a.length-1; i++){
+            if(a[i] != a[i+1]){
+                a[j] = a[i];
+                j++;
+                
+            }
+           
+        }
+        for(int i=0; i<j;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+    
+}
